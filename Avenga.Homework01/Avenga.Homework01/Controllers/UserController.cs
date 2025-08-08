7a8dc434064f46f349342a6561829d30da561641
Darko2504 <darkomilanovski45@gmail.com>
2025-08-08 17:11:52 +0200

    First Homework added

---
+++ Avenga.Homework01/Avenga.Homework01/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Avenga.Homework01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get() 
+        { 
+            return Ok(StaticDb.Users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetById(int id) 
+        {
+            try
+            {
+                if (id < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest,
+                        "The id has negative value");
+                }
+                if (id > StaticDb.Users.Count())
+                {
+                    return StatusCode(StatusCodes.Status404NotFound,
+                        $"There is no resource on index {id}");
+                }
+
+                return StatusCode(StatusCodes.Status200OK, StaticDb.Users[id]);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"An error occurred. Contact the admin {e.Message}");
+            }
+        }
+    }
+}
---
+++ Avenga.Homework01/Avenga.Homework01/StaticDb.cs
@@ -0,0 +1,13 @@
+﻿namespace Avenga.Homework01
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "Darko",
+            "Marko",
+            "Mario",
+            "Angel"
+        };
+    }
+}
